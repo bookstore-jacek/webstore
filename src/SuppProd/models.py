88b0f71ec77c70492c6fa010d9660d3df8d767db
Zwo1in <zwolin13@gmail.com>
2019-06-02 22:11:58 +0200

    resolver SuppProd error

--- src/SuppProd/models.py
+++ src/SuppProd/models.py
@@ -4,7 +4,7 @@ from Product.models import Product
 # Create your models here.
 class SuppProd(models.Model):
     product = models.ForeignKey(Product, models.DO_NOTHING, blank=True, null=True)
-    supplier = models.ForeignKey('Supplier', models.DO_NOTHING, blank=True, null=True)
+    supplier = models.ForeignKey('Supplier.Supplier', models.DO_NOTHING, blank=True, null=True)
 
     class Meta:
         db_table = 'supp_prod'

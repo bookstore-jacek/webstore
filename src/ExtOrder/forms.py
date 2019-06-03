d0e02468ce3d5f0f099dc5d893dad545a9474e5d
Zwo1in <zwolin13@gmail.com>
2019-06-04 00:32:36 +0200

    initial vommit, need to do again

--- src/ExtOrder/forms.py
+++ src/ExtOrder/forms.py
@@ -2,38 +2,19 @@ from django import forms
 from .models import ExtOrder as Order
 
 class CustomerForm(forms.Form):
-    text = forms.CharField(label='', required=True, widget=forms.TextInput(attrs={ "placeholder":"Wyszukaj klienta *", "class": "input_field"}))
+    text = forms.CharField(label='', required=False, widget=forms.TextInput(attrs={ "placeholder":"Wyszukaj klienta *", "class": "input_field"}))
 
-    
+class CustomerRadioForm(forms.Form):
+    def __init__(self, customers, *args, **kwargs):
+        super(CustomerRadioForm, self).__init__(*args, **kwargs)
+        CHOICES = [(cust.id, f"{cust.first_name} {cust.last_name}") for cust in customers[:8]]
+        self.fields['choosen_customer'] = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, label='')
 
-# class OrderForm(forms.ModelForm):
-#     CHOICES=[('select1','select 1'),
-#             ('select2','select 2')]
-#     paid = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect)
-    
-#     class Meta:
-#         model = Order
-#         fields = [
-#             'paid',
-#         ]   
-    
-#     # def clean_customer(self, *args, **kwargs):
-#     #     email= self.cleaned_data.get("email")
-#     #     if None in email:
-#     #         return None
+class ProductForm(forms.Form):
+    text = forms.CharField(label='', required=False, widget=forms.TextInput(attrs={ "placeholder":"Wyszukaj produkt *", "class": "input_field"}))
 
-#     def clean_email(self):
-#         # Get the email
-#         email = self.cleaned_data.get('email')
-#         if email == "" :
-#             return None
-
-#         # Check to see if any users already exist with this email as a username.
-#         try:
-#             match = Customer.objects.get(email=email)
-#         except Customer.DoesNotExist:
-#             # Unable to find a user, this is fine
-#             return email
-
-#         # A user was found with this as a username, raise an error.
-#         raise forms.ValidationError('Wprowadzony e-mail już istnieje.')
+class ProductRadioForm(forms.Form):
+    def __init__(self, products, *args, **kwargs):
+        super(ProductRadioForm, self).__init__(*args, **kwargs)
+        CHOICES = [(prod.id, prod.name) for prod in products[:8]]
+        self.fields['choosen_product'] = forms.ChoiceField(choices=CHOICES, widget=forms.RadioSelect, label='')
--- src/ExtOrder/views.py
+++ src/ExtOrder/views.py
@@ -4,41 +4,116 @@ from django.views import View
 from .models import ExtOrder as Order
 from OrderedProduct.models import OrderedProduct
 from Product.models import Product
-from .forms import CustomerForm
+from .forms import CustomerForm, CustomerRadioForm, ProductForm, ProductRadioForm
 from Customer.models import Customer
+from Product.models import Product
+from SuppProd.models import SuppProd
+from Supplier.models import Supplier
 
 from functools import reduce
 # Create your views here.
-
-def find_customers(some_data):
-    text = some_data.strip().split(' ')
+class Orders_add_view(View):
+    template_name = "order/add_order.html"
     customers = []
-    for n, word in enumerate(text):
-        customers.append(set())
-        try:
-            num = int(word)
-            customers[n].update([customer for customer in Customer.objects.filter(phone__contains=num)])
-        except ValueError:
-            pass
-        customers[n].update([customer for customer in Customer.objects.filter(first_name__icontains=word)])
-        customers[n].update([customer for customer in Customer.objects.filter(last_name__icontains=word)])
-        customers[n].update([customer for customer in Customer.objects.filter(email__icontains=word)])
-        for cust in customers[n]:
-            print(n, cust)
-    customers = list(reduce(lambda x, y: x & y, customers))
-    return customers
+    products = []
+    customer_found = False
+    customer_added = False
+    choosen_cust = None
+    choosen_prods = []
+    product_found = False
+    product_added = False
 
-def add_order_view(request):
-    form = CustomerForm(request.POST or None)
-    customers = []
-    if form.is_valid():
-        customers = find_customers(form.cleaned_data.get('text'))
+    def find_customers(self, some_data):
+        text = some_data.strip().split(' ')
+        customers = []
+        for n, word in enumerate(text):
+            customers.append(set())
+            try:
+                num = int(word)
+                customers[n].update([customer for customer in Customer.objects.filter(phone__contains=num)])
+            except ValueError:
+                pass
+            customers[n].update([customer for customer in Customer.objects.filter(first_name__icontains=word)])
+            customers[n].update([customer for customer in Customer.objects.filter(last_name__icontains=word)])
+            customers[n].update([customer for customer in Customer.objects.filter(email__icontains=word)])
 
-    context={
-        "form": form,
-        "customers": customers
-    }
-    return render(request, "order/add_order.html", context)
+        self.customers = list(reduce(lambda x, y: x & y, customers))
+    
+    def find_products(self, some_data):
+        text = some_data.strip().split(' ')
+        products = []
+        for n, word in enumerate(text):
+            products.append(set())
+            products[n].update([product for product in Product.objects.filter(name__icontains=word)])
+        
+        self.products = list(reduce(lambda x, y: x & y, products))
+
+    def get(self, request, *args, **kwargs):
+        customer_form = CustomerForm()
+        product_form = ProductForm()
+
+        context={
+            "customer_form": customer_form,
+            "cust_found": self.customer_found,
+            "cust_added": self.customer_added,
+            "choosen_cust": self.choosen_cust,
+            "product_form": product_form,
+            "choosen_prods": self.choosen_prods
+        }
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        if 'first' in request.POST:
+            customer_form = CustomerForm(request.POST)
+            if customer_form.is_valid():
+                self.customer_found = True
+                self.find_customers(customer_form.cleaned_data.get('text'))
+        else:
+            customer_form = CustomerForm()
+    
+        if 'second' in request.POST:
+            choosen_id = request.POST.get('choosen_customer')
+            customer_radio_form = CustomerRadioForm([Customer.objects.get(id=choosen_id)], request.POST)
+            if customer_radio_form.is_valid():
+                self.customer_added = True
+                self.choosen_cust = Customer.objects.get(id=choosen_id)
+        else:
+            customer_radio_form = CustomerRadioForm(self.customers)
+
+        if 'third' in request.POST:
+            product_form = ProductForm(request.POST)
+            if product_form.is_valid():
+                self.product_found = True
+                self.customer_added = True
+                self.customer_found = True
+                self.find_products(product_form.cleaned_data.get('text'))
+                print(self.product_found, self.customer_added, self.customer_found)
+        else:
+            product_form = ProductForm()
+        
+        if 'fourth' in request.POST:
+            choosen_id = request.POST.get('choosen_product')
+            product_radio_form = ProductRadioForm([Product.objects.get(id=choosen_id)], request.POST)
+            if product_radio_form.is_valid():
+                self.product_added = True
+                self.choosen_prods += [Product.objects.get(id=choosen_id)]
+                print(self.customer_added, self.customer_found)
+        else:
+            print(self.products)
+            product_radio_form = ProductRadioForm(self.products)
+
+        context={
+            "customer_form": customer_form,
+            "customer_radio_form": customer_radio_form,
+            "cust_found": self.customer_found,
+            "cust_added": self.customer_added,
+            "choosen_cust": self.choosen_cust,
+            "product_form": product_form,
+            "product_radio_form": product_radio_form,
+            "choosen_prods": self.choosen_prods
+        }
+        return render(request, self.template_name, context)
+
 
 class Orders_list_view(View):
     template_name = "order/all_orders.html"
@@ -56,6 +131,7 @@ class Orders_list_view(View):
             'orders': self.get_queryset()
         }
         return render(request, self.template_name, context)
+
 
 def find_order_view(request, *args, **kwargs):
     return render(request, "order/find_order.html", {})

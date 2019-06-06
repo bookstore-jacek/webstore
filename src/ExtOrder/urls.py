5152582db3b7f0e0ddd5b1748787659e420fa28d
lukilake <jezioro73@gmail.com>
2019-06-06 07:42:12 +0200

    .

--- src/ExtOrder/urls.py
+++ src/ExtOrder/urls.py
@@ -10,3 +10,13 @@ urlpatterns = [
     path('<int:id>/', detail_view, name='order-detail'),
     path('<int:id>/edytuj', edit_view, name='order-edit'),
 ]
+
+# from django.conf.urls import url
+
+# from qr_code import views
+
+
+# app_name = 'qr_code'
+# urlpatterns = [
+#     url(r'^images/serve_qr_code_image/$', qr_code.views.serve_qr_code_image, name='serve_qr_code_image')
+# ]
